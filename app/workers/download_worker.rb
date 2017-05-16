71dfc5646dcc9c8573fc669c57c35f14129fe310
Kai Marshland <kaimarshland@gmail.com>
2017-05-15 22:48:07 -0700

    Added thread pool and already downloaded check

--- app/workers/download_worker.rb
+++ app/workers/download_worker.rb
@@ -3,6 +3,11 @@ require 'fileutils'
 class DownloadWorker
   include Sidekiq::Worker
 
+  PREDICTION_MAX_HOURS = 384
+  PREDICTION_PERIODS = %w(0000 0600 1200 1800)
+  HOUR_RESOLUTION = 3
+  THREAD_POOL_SIZE = (ENV['DOWNLOAD_POOL_SIZE'] || 50).to_i
+
   # downloads the dataset for the given day (will look in the past if it can't find it)
   def perform(at=DateTime.now)
 
@@ -31,8 +36,6 @@ class DownloadWorker
   end
 
   # downloads a dataset from a folder definition
-  PREDICTION_MAX_HOURS = 384
-  HOUR_RESOLUTION = 3
   def download(url)
     at = DateTime.strptime(url.split('/').last, '%Y%m%d')
     dir = Rails.root.join('lib', 'data', at.strftime('%Y%m%d'))
@@ -40,33 +43,68 @@ class DownloadWorker
     FileUtils::mkdir_p dir
 
     start = DateTime.now
-    threads = []
+    total = (PREDICTION_MAX_HOURS/HOUR_RESOLUTION).ceil * PREDICTION_PERIODS.count
+
+    # build the queue of datasets to download
+    datasets = Queue.new
     number_completed = 0
 
-    (0..PREDICTION_MAX_HOURS).step(HOUR_RESOLUTION).each do |hour_offset|
-      threads << Thread.new("#{url}gfs_4_#{at.strftime('%Y%m%d')}_0000_#{hour_offset.to_s.rjust(3, '0')}.grb2") do |file_url|
-        download_file file_url, dir
-
-        number_completed += 1
-        percentage = (100*number_completed/PREDICTION_MAX_HOURS.to_f/HOUR_RESOLUTION)
-        elapsed = (DateTime.now - start).to_f * 1.day
-        puts "#{percentage.round(1)}% complete (#{elapsed}s elapsed, #{0.01 * elapsed * (100 - percentage)}s remaining)" if hour_offset % 15 == 0
+    PREDICTION_PERIODS.each do |period|
+      (0..PREDICTION_MAX_HOURS).step(HOUR_RESOLUTION).each do |hour_offset|
+        datasets << "#{url}gfs_4_#{at.strftime('%Y%m%d')}_#{period}_#{hour_offset.to_s.rjust(3, '0')}.grb2"
       end
     end
 
-    threads.map(&:join)
+    # make a pool to download them
+    threads = [THREAD_POOL_SIZE, datasets.size].min
+    workers = []
 
+    threads.times do
+      workers << Thread.new do
+        begin
+          while (file_url = datasets.pop(true)).present?
+            download_file file_url, dir
+
+            number_completed += 1
+
+            if number_completed % (total / 10).to_i == 0
+              percentage = (100*number_completed/total.to_f)
+              elapsed = (DateTime.now - start).to_f * 1.day
+              puts "#{percentage.round(1).to_s.rjust(5)}% complete (#{elapsed}s elapsed, #{0.01 * elapsed * (100 - percentage)}s remaining)"
+            end
+          end
+        rescue ThreadError
+        end
+      end
+    end
+
+    workers.map(&:join)
+
+    # logs!
     elapsed = (DateTime.now - start).to_f * 1.day
-    puts "#{elapsed}s to download #{url.split('/').last}".green
+    puts "#{elapsed}s to download #{url.split('/').last} (#{total} checked)".green
   end
 
   # downloads a specific dataset
+  # returns how long it took to download, in seconds or nil if it didn't download
   def download_file(dataset_url, into, debug: false)
+    puts "Starting downloading #{dataset_url.split('/').last}" if debug
     start = DateTime.now
 
-    body = HTTP.get(dataset_url).body
+    filename = into.join(dataset_url.split('/').last)
+    if File.exists?(filename) && File.size(filename) > 1.megabyte
+      return
+    end
 
-    File.open(into.join(dataset_url.split('/').last), 'wb') do |file|
+
+    response = HTTP.get(dataset_url)
+    unless response.code == 200
+      return
+    end
+
+    body = response.body
+
+    File.open(filename, 'wb') do |file|
       body.each do |data|
         file.write data
       end
@@ -74,6 +112,8 @@ class DownloadWorker
 
     elapsed = (DateTime.now - start).to_f * 1.day
     puts "#{elapsed}s to download #{dataset_url.split('/').last}" if debug
+
+    elapsed
   end
 
 end
