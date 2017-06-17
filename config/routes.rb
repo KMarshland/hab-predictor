846b993144b6578df3b2f0aae8d2738d6628e820
Zach Furman <zach.furman1@gmail.com>
2017-06-17 00:57:36 -0700

    Added guidance controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'predict' => 'prediction#predict'
+  get 'guidance' => 'guidance#guidance'
 
   root 'status#status'
 end
