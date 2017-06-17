1f4db5711e7de3bfcb2934fc0856d273fc4f5bb2
Zach Furman <zach.furman1@gmail.com>
2017-06-17 01:00:43 -0700

    Deleted unnecessary files used with Tawhiri

--- lib/prediction/dataset_downloader.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-../../../tawhiri-downloader/_build/main.native daemon > /dev/null 2>&1 &
--- lib/prediction/interface_server.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Activates the python virtualenv, then the interface server
-source '../../../tawhiri/venv/bin/activate'
-python 'habmc_interface.py'
-deactivate
