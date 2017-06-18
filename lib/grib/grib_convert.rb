53603356c9b4fbeac7f08eaddf2a90207db7c113
Kai Marshland <kaimarshland@gmail.com>
2017-06-17 21:20:45 -0700

    Condensed pre-processed format

--- lib/grib/grib_convert.rb
+++ lib/grib/grib_convert.rb
@@ -1,68 +1,99 @@
 require 'fileutils'
 
 module GribConvert
-  LEVELS = [1, 2, 3, 5, 7, 10, 20, 30, 50, 70, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 925, 950, 975, 1000]
-  CELL_SIZE = 25 # we don't want to have too many open files. Limit defaults to 256
-  BUFFER_SIZE = 4096 # in units of bytes
+  LEVELS = [1, 2, 3, 5, 7, 10, 20, 30, 50, 70, 80, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 925, 950, 975, 1000]
+  CELL_SIZE = 25 # lower means the predictor runs faster, higher means this thing does
+  MEMORY_BUFFER_SIZE = 100_000 # in units of velocity tuple count (approx 20 bytes each)
+  APPROX_TUPLE_COUNT = 10_500_000
 
   class << self
-    def convert_level(level:, path:, dir:)
-      base_dir = "#{dir}/L#{level}"
-      FileUtils::mkdir_p base_dir
+    def read_to_format(path:, dir:)
+      return if File.exist? "#{dir}/.done" # don't reparse
 
-      return if File.exist? "#{base_dir}/.done" # don't reparse
+      # TODO: clear old files
 
-      # pre-open files
-      files = {}
-      buffers = {}
+      # each buffer tuple is an array [level, lat, lon, u, v]
+      incomplete_buffer = {}
+      complete_buffer = []
 
-      (-90..90).step(CELL_SIZE).each do |lat|
-        grid_lat = (lat / CELL_SIZE).floor * CELL_SIZE
+      # flushes existing
+      written = 0
+      flush_buffer = -> {
+        file_contents = {}
 
-        files[grid_lat] ||= {}
-        buffers[grid_lat] ||= {}
+        complete_buffer.each_with_index do |vel, index|
+          next if vel.nil?
+          next if vel[3].nil? || vel[4].nil?
 
-        (0..360).step(CELL_SIZE).each do |lon|
-          grid_lon = (lon / CELL_SIZE).floor * CELL_SIZE
-          files[grid_lat][grid_lon] ||= File.open("#{base_dir}/C#{grid_lat}_#{grid_lon}.gribp", 'wb')
-          buffers[grid_lat][grid_lon] ||= ''
+          grid_lat = (vel[1] / CELL_SIZE).floor * CELL_SIZE
+          grid_lon = (vel[2] / CELL_SIZE).floor * CELL_SIZE
+
+          filename = "L#{vel[0]}/C#{grid_lat}_#{grid_lon}.gribp"
+          file = (file_contents[filename] ||= '')
+
+          # convert the buffer to strings
+          file << "#{[vel[1]].pack('g')}#{[vel[2]].pack('g')}#{[vel[3]].pack('g')}#{[vel[4]].pack('g')}"
+
+          # don't rewrite the same data
+          complete_buffer[index] = nil
+          written += 1
         end
-      end
+
+        # write the buffer to files
+        file_contents.each do |name, contents|
+
+          FileUtils::mkdir_p "#{dir}/#{name.match(/L\d+/).to_s}"
+
+          # TODO: this may actually clobber the some data. We need to check file mode
+          file = File.open("#{dir}/#{name}", 'wb')
+          file.write contents
+
+          file.close
+        end
+
+        complete_buffer = []
+        puts "\t #{written} tuples written (~#{(100.0*written/APPROX_TUPLE_COUNT.to_f).round(2)}%)" if written > 0
+      }
 
       # start parsing
-      command = "grib_get_data -p shortName -w level=#{level} #{path}"
+      command = "grib_get_data -p shortName,level -w shortName=u/v,level!=0 #{path}"
 
       IO.popen(command) do |io|
         io.gets # skip first line
         while (line = io.gets) do
-          lat, lon, value, label = line.split ' '
-          next unless label == 'u' || label == 'v'
+          lat, lon, value, label, level = line.split ' '
           next if lat == '' || lon == '' || value == ''
 
           lat = lat.to_f
           lon = lon.to_f
 
-          grid_lat = (lat / CELL_SIZE).floor * CELL_SIZE
-          grid_lon = (lon / CELL_SIZE).floor * CELL_SIZE
+          key = :"#{level}_#{lat}_#{lon}"
+          buffer = (incomplete_buffer[key] ||= [
+              level,
+              lat,
+              lon,
+              nil,
+              nil
+          ])
 
-          buffer = buffers[grid_lat][grid_lon]
-          buffer << "#{[lat].pack('g')}#{[lon].pack('g')}#{[value.to_f].pack('g')}#{label[0]}"
-
-          if buffer.length > BUFFER_SIZE
-            files[grid_lat][grid_lon].write buffer
-            buffers[grid_lat][grid_lon] = ''
+          if label == 'u'
+            buffer[3] = value.to_f
+          else
+            buffer[4] = value.to_f
           end
 
+          if buffer[3] && buffer[4]
+            complete_buffer << incomplete_buffer.delete(key)
+          end
+
+          flush_buffer[] if complete_buffer.size >= MEMORY_BUFFER_SIZE
         end
+
       end
 
-      files.each do |_, file_list|
-        file_list.each do |_, file|
-          file.close
-        end
-      end
+      flush_buffer[]
 
-      FileUtils::touch "#{base_dir}/.done"
+      FileUtils::touch "#{dir}/.done"
     end
 
     def convert(path)
@@ -74,14 +105,7 @@ module GribConvert
 
       FileUtils::mkdir_p base_dir
 
-      # Note: multithreading this does not help as it must do IO on the same input files
-      LEVELS.reverse_each do |level|
-        start = Time.now
-
-        convert_level(path: path, level: level, dir: base_dir)
-
-        puts "    converted L#{level} (#{(Time.now - start).round(2)}s)"
-      end
+      read_to_format(dir: base_dir, path: path)
 
       seconds = Time.now - true_start
       puts "-> Converted #{LEVELS.length} levels (#{seconds.round(2)}s, #{(seconds / LEVELS.length.to_f).round(2)}s avg)"
