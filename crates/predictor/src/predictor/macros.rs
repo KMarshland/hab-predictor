bef6ede0c0157c553760403420bd53c15a0c8879
Kai Marshland <kaimarshland@gmail.com>
2017-08-17 23:06:50 -0700

    Made intialization safer in the case of no data

--- crates/predictor/src/predictor/macros.rs
+++ crates/predictor/src/predictor/macros.rs
@@ -42,3 +42,54 @@ macro_rules! unsafe_dereference {
         }
     };
 }
+
+/*
+ * let a = result_or_return_why!(b, "Because b")
+ *
+ * -------
+ *
+ * let a = match b {
+ *      Ok(b) => {
+ *          b
+ *      },
+ *      Err(why) => {
+ *          return Err(String::from("Because b"))
+ *      }
+ * }
+ */
+macro_rules! result_or_return_why {
+    ($variable:expr, $why:expr) => {
+        match $variable {
+            Ok(value) => value,
+            Err(_) => {
+                return Err(String::from($why))
+            }
+        }
+    };
+}
+
+
+/*
+ * let a = some_or_return_why!(b, "Because b")
+ *
+ * -------
+ *
+ * let a = match b {
+ *      Some(b) => {
+ *          b
+ *      },
+ *      None => {
+ *          return Err(String::from("Because b"))
+ *      }
+ * }
+ */
+macro_rules! some_or_return_why {
+    ($variable:expr, $why:expr) => {
+        match $variable {
+            Some(value) => value,
+            None => {
+                return Err(String::from($why))
+            }
+        }
+    };
+}
