806dedc98f0227d0f2850d02491e32510366f0fd
Kai Marshland <kaimarshland@gmail.com>
2017-09-21 22:12:16 -0700

    Made it able to call the grib api

--- preprocessor/src/preprocessor/preprocess.rs
+++ preprocessor/src/preprocessor/preprocess.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::path;
 use std::fs;
 use std::ffi::OsStr;
+use std::process::{Command, Stdio};
+use std::io::{BufReader, BufRead};
 
 use chrono::prelude::*;
 
@@ -18,6 +20,9 @@ pub fn preprocess(at : DateTime<Utc>) -> Result<(), PreprocessorError> {
         return Err(PreprocessorError::InvalidDatasetError);
     }
 
+    let processed_dir = env::current_dir().unwrap().join("data").join("processed").join(at.format("%Y%m%d").to_string());
+    fs::create_dir_all(&processed_dir)?;
+
     for entry in fs::read_dir(raw_data_directory)? {
         let path = entry?.path();
         let extension = path.extension();
@@ -26,7 +31,8 @@ pub fn preprocess(at : DateTime<Utc>) -> Result<(), PreprocessorError> {
             continue;
         }
 
-        process_file(&path)?;
+        process_file(&path, &processed_dir)?;
+        break
     }
 
     Ok(())
@@ -35,8 +41,42 @@ pub fn preprocess(at : DateTime<Utc>) -> Result<(), PreprocessorError> {
 /*
  * Processes the provided grib file
  */
-fn process_file(file : &path::PathBuf) -> Result<(), PreprocessorError> {
+fn process_file(file : &path::PathBuf, processed_dir : &path::PathBuf) -> Result<(), PreprocessorError> {
     println!("Processing {}", file.to_string_lossy());
+
+    let output_dir = processed_dir.join(file.file_stem().unwrap());
+    let partial_dir = (&output_dir).with_extension("partial");
+
+    // remove any partial data in order to reprocess
+    if (&partial_dir).exists() {
+        fs::remove_dir_all(&output_dir.with_extension("partial"))?;
+    }
+
+    // recreate the partial directory
+    fs::create_dir_all(&partial_dir)?;
+
+    let mut process = Command::new("grib_get_data")
+        .arg("-p").arg("shortName,level")
+        .arg("-w").arg("shortName=u/v/t,level!=0")
+        .arg(file)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn().unwrap();
+
+    let reader = BufReader::new(process.stdout.unwrap());
+
+    let mut lines_iter = reader.lines();
+    loop {
+        match lines_iter.next() {
+            Some(line) => {
+
+            },
+            None => {
+                println!("None!");
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
